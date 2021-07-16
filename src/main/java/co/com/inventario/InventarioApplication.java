993999011e3a37813fc5e174cd772a8db324a803
ECM1256Q <ECM1256Q@comcel.com.co>
2021-07-15 23:53:15 -0500

    Ajustes de CORS

--- src/main/java/co/com/inventario/InventarioApplication.java
+++ src/main/java/co/com/inventario/InventarioApplication.java
@@ -3,7 +3,10 @@ package co.com.inventario;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @ComponentScan({"co.com.*"})
 @EntityScan(basePackages = {"co.com.inventario.model"})
@@ -12,6 +15,16 @@ public class InventarioApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InventarioApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST","PUT", "DELETE");
+			}
+		};
 	}
 
 }
